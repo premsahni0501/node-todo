5824832c9858d3d280deafea036b8c2a33b7c9a6
Prem Chand <premsahni0501@gmail.com>
2019-03-30 23:19:32 +0530

    mlab db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: "mongodb://premsahni0501@gmail.com:Prem0501@mlab@ds229186.mlab.com:29186/node-todo",
+    db: "mongodb://premsahni:DhAUZeJ84U9vF9r@ds229186.mlab.com:29186/node-todo",
     secret: "myscret"
 }
