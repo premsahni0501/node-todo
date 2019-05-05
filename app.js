cf5856478c823a7b79967d8b0435a0f4ebdef31f
Prem Chand <premsahni0501@gmail.com>
2019-05-05 21:03:04 +0530

    base href

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/react', function(req, res){
 userModel(mongoose);
 todoModel(mongoose);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port);
 console.log("Server is listening on port "+port);
 
